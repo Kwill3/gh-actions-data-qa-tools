6d22cac816d586bb8b4ed6dfec38e6e91c7c8087
William Lee <wei.yanglee@kyd.ai>
2023-04-20 10:51:06 +0100

    Code slimming with DictReader

--- dupe_key_check.py
+++ dupe_key_check.py
@@ -20,38 +20,28 @@ for file in csv_files:
     dupe_list = {}
 
     with open(CSV_PATH, 'r', encoding='utf-8') as csv_in:
-        csv_reader = csv.reader(csv_in, delimiter=',')
+        csv_reader = csv.DictReader(csv_in, delimiter=',')
         line_count = 0
-        key_index = start_index = end_index = None
         for row in csv_reader:
-            if line_count == 0:
-                for i, items in enumerate(row):
-                    if items == KEY_COLUMN_NAME:
-                        key_index = i
-                    elif items == START_COLUMN_NAME:
-                        start_index = i
-                    elif items == END_COLUMN_NAME:
-                        end_index = i
-            else:
-                # Check if key exists
-                key_value = row[key_index]
-                start_value = row[start_index]
-                end_value = row[end_index]
-                if key_value in key_check:
-                    # Check if same start and end exists and is a duplicate
-                    if [start_value, end_value] in key_check[key_value]['start_end']:
-                        key_check[key_value]['line'].append(line_count)
-                        dupe_count += 1
-                    else:
-                        key_check[key_value]['start_end'].append([start_value, end_value])
+            # Check if key exists
+            key_value = row[KEY_COLUMN_NAME]
+            start_value = row[START_COLUMN_NAME]
+            end_value = row[END_COLUMN_NAME]
+            if key_value in key_check:
+                # Check if same start and end exists and is a duplicate
+                if [start_value, end_value] in key_check[key_value]['start_end']:
+                    key_check[key_value]['line'].append(line_count)
+                    dupe_count += 1
                 else:
-                    key_check.update({
-                        key_value: {
-                            'start_end': [[start_value, end_value]],
-                            'line': []
-                        }
-                    })
-            line_count += 1
+                    key_check[key_value]['start_end'].append([start_value, end_value])
+            else:
+                key_check.update({
+                    key_value: {
+                        'start_end': [[start_value, end_value]],
+                        'line': []
+                    }
+                })
+        line_count += 1
 
         # Creates a list of only duplicates with their indices
         for key, value in key_check.items():
