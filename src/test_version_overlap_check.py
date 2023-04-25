cb12ae9fc7e0a95cfb68c6979010ff4c29e80dac
William Lee <wei.yanglee@kyd.ai>
2023-04-25 14:19:36 +0100

    Project structure setup for tests

--- src/test_version_overlap_check.py
+++ src/test_version_overlap_check.py
@@ -6,15 +6,12 @@
 """
 
 import os
-import sys
 from version_overlap_check import scan_file
 
 BASE_PATH = os.path.abspath('.')
-sys.path.insert(0, os.path.abspath('./src'))
-print (sys.path)
 
 def test_scenario1():
     test1 = {}
-    count = scan_file(BASE_PATH + '/src/tests/test_scenario1.csv', test1)
+    count = scan_file(BASE_PATH + '/tests/test_scenario1.csv', test1)
     print(test1)
     assert count == 6
