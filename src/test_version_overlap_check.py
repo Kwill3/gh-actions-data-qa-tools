5de6c96ac49a0bd7a30fb7b4b4d67b7a53d640ce
William Lee <39934951+Kwill3@users.noreply.github.com>
2023-04-28 09:01:32 +0000

    Det 143 py test to check python files (#2)
    * DET-143 Modularize python code
    
    * DET-143 Test scenario 1
    
    * DET-143 Fix bugs discovered from unit tests
    
    * DET-143 Simplified checks for current version
    
    * DET-143 Add test scenarios
    
    * DET-143 Return output as tuple
    
    * DET-143 Code cleanup
    
    ---------
    
    Co-authored-by: William Lee <wei.yanglee@kyd.ai>

--- src/test_version_overlap_check.py
+++ src/test_version_overlap_check.py
@@ -11,7 +11,161 @@ from version_overlap_check import scan_file
 BASE_PATH = os.path.abspath('.')
 
 def test_scenario1():
-    test1 = {}
-    count = scan_file(BASE_PATH + '/tests/test_scenario1.csv', test1)
-    print(test1)
-    assert count == 6
+    """Passing test for normal back-to-back versions
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario1.csv')
+    assert output == (0, {})
+
+def test_scenario2():
+    """Passing test for first instance of current version
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario2.csv')
+    assert output == (0, {})
+
+def test_scenario3():
+    """Passing test for first instance of start and end version
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario3.csv')
+    assert output == (0, {})
+
+def test_scenario4():
+    """Failing test for [ (] ) overlap
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario4.csv')
+    assert output == (6, {
+        'UK': {1: ['2', '4'], 2: ['1', '3']},
+        'US': {3: ['3', '7'], 4: ['1', '5']},
+        'FR': {5: ['2', '3'], 6: ['1', '3']}
+    })
+
+def test_scenario5():
+    """Failing test for ( [) ] overlap
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario5.csv')
+    assert output == (6, {
+        'UK': {1: ['1', '3'], 2: ['2', '4']},
+        'US': {3: ['1', '5'], 4: ['3', '7']},
+        'FR': {5: ['1', '3'], 6: ['2', '3']}
+    })
+
+def test_scenario6():
+    """Failing test for ([ ]) overlap
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario6.csv')
+    assert output == (9, {
+        'UK': {1: ['1', '4'], 2: ['2', '3']},
+        'US': {3: ['2', '10'], 4: ['4', '8'], 5: ['5', '6']},
+        'FR': {6: ['1', '10'], 7: ['8', '10']},
+        'SG': {8: ['2', '10'], 9: ['2', '7']}
+    })
+
+def test_scenario7():
+    """Failing test for [( )] overlap
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario7.csv')
+    assert output == (9, {
+        'UK': {1: ['2', '3'], 2: ['1', '4']},
+        'US': {3: ['5', '6'], 4: ['4', '8'], 5: ['2', '10']},
+        'FR': {6: ['8', '10'], 7: ['1', '10']},
+        'SG': {8: ['2', '7'], 9: ['2', '10']}
+    })
+
+def test_scenario8():
+    """Failing test for current version overlap with new current version
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario8.csv')
+    assert output == (9, {
+        'UK': {1: ['1', 99999], 2: ['1', 99999]},
+        'US': {3: ['1', 99999], 4: ['22', 99999]},
+        'FR': {5: ['2', 99999], 6: ['5', 99999]},
+        'SG': {7: ['1', 99999], 8: ['33', 99999], 9: ['2', 99999]}
+    })
+
+def test_scenario9():
+    """Failing test for current version overlap before start of ranges (range first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario9.csv')
+    assert output == (6, {
+        'UK': {1: ['10', '11'], 2: ['1', 99999]},
+        'US': {3: ['4', '5'], 4: ['3', 99999]},
+        'FR': {5: ['12', '20'], 6: ['12', 99999]}
+    })
+
+def test_scenario10():
+    """Failing test for current version overlap before start of ranges (current version first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario10.csv')
+    assert output == (6, {
+        'UK': {1: ['1', 99999], 2: ['10', '11']},
+        'US': {3: ['3', 99999], 4: ['4', '5']},
+        'FR': {5: ['12', 99999], 6: ['12', '20']}
+    })
+
+def test_scenario11():
+    """Failing test for current version overlap after start of ranges (range first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario11.csv')
+    assert output == (4, {
+        'US': {3: ['4', '8'], 4: ['7', 99999]},
+        'FR': {5: ['12', '20'], 6: ['13', 99999]}
+    })
+
+def test_scenario12():
+    """Failing test for current version overlap after start of ranges (current version first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario12.csv')
+    assert output == (4, {
+        'US': {3: ['7', 99999], 4: ['4', '8']},
+        'FR': {5: ['13', 99999], 6: ['12', '20']}
+    })
+
+def test_scenario13():
+    """Passing test for having a range before a current version (range first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario13.csv')
+    assert output == (0, {})
+
+def test_scenario14():
+    """Passing test for having a range before a current version (current version first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario14.csv')
+    assert output == (0, {})
+
+def test_scenario15():
+    """Passing test for having a range before a current version (current version first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario15.csv')
+    assert output == (5, {
+        'UK': {3: ['7', 99999], 4: ['10', '11']},
+        'FR': {8: ['1', '5'], 9: ['2', '3'], 11: ['3', '4']}
+    })
+
+def test_scenario16():
+    """Passing test for having a range before a current version (current version first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario16.csv')
+    assert output == (2, {
+        'UK': {4: 'Invalid start value'},
+        'FR': {9: 'Invalid start value'}
+    })
+
+def test_scenario17():
+    """Passing test for having a range before a current version (current version first)
+    """
+    output = scan_file(BASE_PATH + '/tests/test_scenario17.csv')
+    assert output == (2, {
+        'UK': {4: 'Start value greater than end value'},
+        'FR': {9: 'Start value greater than end value'}
+    })
+
+# def test_scenario():
+#     """Test for passing case, normal back-to-back versions
+#     """
+#     test = 0
+#     count = scan_file(BASE_PATH + '/tests/test_scenario.csv', test)
+#     assert count == {
+#         'UK': {},
+#         'US': {},
+#         'FR': {},
+#         'SG': {}
+#     }
--- src/version_overlap_check.py
+++ src/version_overlap_check.py
@@ -4,13 +4,17 @@
 
 """
 import csv
-import sys
 
-check_failed_flag = False
-failed_files = []
-def scan_file(filename:str, overlap_list)->int:
-    # Input settings
-    CSV_PATH = filename
+def scan_file(filename:str) -> tuple[int, dict]:
+    """Checks input filename for overlaps in start and end versions
+
+    Args:
+        filename (str): name of the input file
+
+    Returns:
+        tuple: returns a count (int) and key + index of overlaps (dict)
+    """
+    # TODO: Implement getopt options for input settings
     KEY_COLUMN_NAME = 'key_id'
     START_COLUMN_NAME = 'startv'
     END_COLUMN_NAME = 'endv'
@@ -19,82 +23,68 @@ def scan_file(filename:str, overlap_list)->int:
     overlap_count = 0
     overlap_list = {}
 
-    with open(CSV_PATH, 'r', encoding='utf-8') as csv_in:
+    def add_to_overlap_list(value:any):
+        """Checks whether to create a new key_value in tracker list or if it exists in tracker list
+        and then add value from the argument according to its key (line count)
+
+        Args:
+            value (any): value to be added to the key
+        """        
+        if overlap_list.get(key_value):
+            overlap_list[key_value].update({
+                line_count: value
+            })
+        else:
+            overlap_list.update({
+                key_value: {
+                    line_count: value
+                }
+            })
+
+    with open(filename, 'r', encoding='utf-8') as csv_in:
         csv_reader = csv.DictReader(csv_in, delimiter=',')
         line_count = 1
         for row in csv_reader:
             key_value = row[KEY_COLUMN_NAME]
             start_value = row[START_COLUMN_NAME]
             end_value = row[END_COLUMN_NAME]
-            # Check if key exists
+            # Check if key exists in checked list
             if key_value in key_check:
-                # Current version case
-                if end_value == '':
-                    # Has existing current version that clashes with new current version
-                    if key_check[key_value]['has_current_version']:
-                        # Add old current version to tracker
-                        if overlap_list.get(key_value):
+                # Check if invalid start version (non-integer value)
+                if not start_value.isdigit():
+                    add_to_overlap_list('Invalid start value')
+                # Current version case (versions that have start values but no end values)
+                elif end_value == '':
+                    # Check if current version overlaps with an existing key in checked list
+                    for k, v in key_check[key_value].items():
+                        if int(start_value) < int(v[1]):
+                            # Add current version to tracker list
+                            add_to_overlap_list([start_value, 99999])
+                            # Add existing key to tracker list
                             overlap_list[key_value].update({
-                                key_check[key_value]['current_version_line']: [start_value, '']
+                                k: v
                             })
-                        else:
-                            overlap_list.update({
-                                key_value: {
-                                    key_check[key_value]['current_version_line']: [start_value, '']
-                                }
-                            })
-                        # Add new current version to tracker and checked list
-                        overlap_list[key_value].update({
-                            line_count: [start_value, '']
-                        })
-                        key_check[key_value]['lines'].update({
-                            line_count: [start_value, '']
-                        })
-                    # Set record as existing current version
-                    else:
-                        key_check[key_value].update({
-                            'has_current_version': True,
-                            'current_version_line': line_count
-                        })
-                        # Check if current version overlaps with an existing key
-                        for k, v in key_check[key_value]['lines'].items():
-                            if start_value < v[1]:
-                                # Add current version to tracker
-                                if overlap_list.get(key_value):
-                                    overlap_list[key_value].update({
-                                        line_count: [start_value, end_value]
-                                    })
-                                else:
-                                    overlap_list.update({
-                                        key_value: {
-                                            line_count: [start_value, end_value]
-                                        }
-                                    })
-                                # Add existing key to tracker
-                                overlap_list[key_value].update({
-                                    k: v
-                                })
+                    # Add current version to checked list
+                    key_check[key_value].update({
+                        line_count: [start_value, 99999]
+                    })
+                # If start version is larger than end version, add to tracker list
+                elif int(start_value) > int(end_value):
+                    add_to_overlap_list('Start value greater than end value')
                 else:
-                    for k, v in key_check[key_value]['lines'].items():
+                    for k, v in key_check[key_value].items():
                         # Check if record is within previous version ranges
-                        if not ((start_value < v[0] and end_value <= v[0]) or (start_value >= v[1] and end_value > v[1]) or (end_value <= v[0] and v[1] == '')):
-                            # Add record to tracker
-                            if overlap_list.get(key_value):
-                                overlap_list[key_value].update({
-                                    line_count: [start_value, end_value]
-                                })
-                            else:
-                                overlap_list.update({
-                                    key_value: {
-                                        line_count: [start_value, end_value]
-                                    }
-                                })
-                            # Add existing key to tracker
+                        if not ((int(start_value) < int(v[0]) and int(end_value) <= int(v[0])) or
+                                (int(start_value) >= int(v[1]) and int(end_value) > int(v[1])) or
+                                (int(end_value) <= int(v[0]) and v[1] == 99999)):
+                            # Add record to tracker list
+                            add_to_overlap_list([start_value, end_value])
+                            # Add existing key to tracker list
                             overlap_list[key_value].update({
                                 k: v
                             })
                     # Add record to checked list
-                    key_check[key_value]['lines'].update({
+                    key_check[key_value].update({
                         line_count: [start_value, end_value]
                     })
             else:
@@ -102,39 +92,25 @@ def scan_file(filename:str, overlap_list)->int:
                 if end_value == '':
                     key_check.update({
                         key_value: {
-                            # TODO: Change end value to consistent format
-                            'lines': {line_count: [start_value, '']},
-                            'has_current_version': True,
-                            'current_version_line': line_count
+                            line_count: [start_value, 99999]
                         }
                     })
                 else:
                     key_check.update({
                         key_value: {
-                            'lines': {line_count: [start_value, end_value]},
-                            'has_current_version': False,
-                            'current_version_line': None
+                            line_count: [start_value, end_value]
                         }
                     })
             line_count += 1
 
+        # Calculate counts for overlaps from tracker list
         print(f'[{filename}]')
-        for key in overlap_list:
-            for entry in key:
-                overlap_count += 1
+        for key in overlap_list.values():
+            overlap_count += len(key)
         print('Number of version overlaps found:', overlap_count)
 
-    # Check for overlaps in this file
+    # Print keys and indices if overlaps exist
     if overlap_count > 0:
         print('List of overlapping versions for keys and their index:', overlap_list)
-        check_failed_flag = True
-        failed_files.append(filename)
 
-    return overlap_count
-
-# Prints run status for file(s)
-if check_failed_flag:
-    print(f"Run ended. Version overlap found in dataset(s): {failed_files}")
-    sys.exit("Checks failed.")
-else:
-    print("Run ended. No overlaps found: Checks passed.")
+    return overlap_count, overlap_list
